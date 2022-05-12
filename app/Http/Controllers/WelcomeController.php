ef05ea44092ef489adfa4836014d1e8eb814a19f
Robin FALCK <robin.falck57@gmail.com>
2022-05-12 17:29:29 +0200

    search function on welcome page

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
+        $products = Product::query();
+        if (request('search')) {
+            $products->where('name', 'Like', '%' . request('search') . '%');
+            return view('mangas', [
+                'products' => $products->orderBy('id', 'ASC')->paginate(16),
+            ]);
+        }
         return view('welcome');
     }
 
     public function contact()
     {
+        $products = Product::query();
+        if (request('search')) {
+            $products->where('name', 'Like', '%' . request('search') . '%');
+            return view('mangas', [
+                'products' => $products->orderBy('id', 'ASC')->paginate(16),
+            ]);
+        }
         return view('contact');
     }
 }
